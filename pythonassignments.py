2701fd2ad1321a2070346fb281990776db3ffc50
KaviyaKrishnasamy-18 <kaviyakrishnasamy18@gmail.com>
2026-06-08 15:47:52 +0530

    Add files via upload

---
+++ pythonassignments.py
@@ -0,0 +1,352 @@
+print("welcome to assignment-1")
+welcome to assignment-1
+num1=10
+num2=30
+add=num1+num2
+print("num1=",num1)
+print("num2=",num2)
+print("add=",add)
+num1= 10
+num2= 30
+add= 40
+num1=10
+num2=30
+add=num1+num2
+print("num1=10")
+print("num2=30")
+print("add=num1+num2")
+num1=10
+num2=30
+add=num1+num2
+bmi=int(input("enter the bmi index"))
+a=63
+b=97
+mul=6111
+a=76
+b=31
+floatdiv=2.04516129032258065
+print("A=76")
+print("b=31")
+print ("floatdiv=2.4516129032258065")
+A=76
+b=31
+floatdiv=2.4516129032258065
+a=76
+b=31
+floordiv=2
+print("a=76")
+print("b=31")
+print("floordiv=2")
+a=76
+b=31
+floordiv=2
+a=76 b=31 modulo=14 print("a=76") print("b=31") print("modulo=14")
+a=4
+b=4
+power=256
+print("a=4")
+print("b=4")
+print("power=256")
+a=4
+b=4
+power=256
+range(0,20)
+range(0, 20)
+print(range(0,20))
+range(0, 20)
+for num in range(0,20):
+    print(num)
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+for num in range(0,20):
+    print(num,end=" ")
+0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 
+for num in range int(10,20,14,55,43,87,76):
+          print("10,20,14,55,43,87,76")
+if(age==18):
+    print("Adult")
+    Adult
+if(age<18):
+    print("children")
+else:
+   print("Adult")
+Adult
+age=int(input("enter the age"))
+enter the age 10
+if(age<18):
+    print("children")
+else:
+    print("Adult")
+children
+age>18-->children
+age<35-->adult
+age<60-->citizen
+age>60-->senior citizen
+age=int(input("enter the age"))
+enter the age 36
+if(age<18):
+    print("children")
+elif(age<35):
+     print("adult")
+elif(age<60):
+     print("citizen")
+else:
+    print("senior citizen")
+citizen
+age=int(input("enter the age"))
+enter the age 4
+if(age<18):
+    print("children")
+elif(age<35):
+     print("adult")
+elif(age<60):
+     print("citizen")
+else:
+    print("senior citizen")
+children
+age=int(input("enter the age"))
+enter the age 60
+if(age<18):
+    print("children")
+elif(age<35):
+     print("adult")
+elif(age<60):
+     print("citizen")
+else:
+    print("senior citizen")
+senior citizen
+lists=[25,36,45,58,61,72,88]
+for temp in lists:
+    print(temp)
+25
+36
+45
+58
+61
+72
+88
+for age in lists:
+   if(age<18):
+    print("children")
+   elif(age<35):
+     print("adult")
+   elif(age<60):
+     print("citizen")
+   else:
+    print("senior citizen")    
+adult
+citizen
+citizen
+citizen
+senior citizen
+senior citizen
+senior citizen
+num=int(input("enter the number:"))
+if((num%2)==0):
+    print("even number")
+else:
+    print("odd number")
+enter the number: 49
+odd number
+num=int(input("enter the number:"))
+if((num%2)==2):
+    print("even number")
+else:
+    print("odd number")
+enter the number: 83
+odd number
+for num in range(0,100):
+    print(num) 
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+class subfieldsinAI():
+    def subfields():
+        print("subfields in AI are:")
+        lists=["machine learning","neural networks","vision","robotics","speech processing","natural language processing"]
+        for item in lists:
+            print(item)
+subfieldsinAI.subfields()
+subfields in AI are:
+machine learning
+neural networks
+vision
+robotics
+speech processing
+natural language processing
+class oddeven():
+    def oddeven():
+        num=int(input("enter the number"))
+        if num%2==0:
+            print(num,"is even number")
+        else:
+            print(num,"is odd number")
+oddeven.oddeven()
+enter the number 50
+50 is even number
+ class eligibilityformarraige():
+        def eligible():
+                gender= input("Your Gender (male/female):")
+                age=int(input("your age"))
+                if gender.lower()=="male":
+                    if age>=21:
+                        print("eligible")
+                else:
+                       print("not eligible")
+                if gender.lower()=="female":
+                 if age>=18:
+                        print("eligible")
+                else:
+                        print("not eligible")
+    eligibilityformarraige.eligible()
+Your Gender (male/female): female
+your age 20
+not eligible
+eligible
+class multiplefunctions():
+    def oddeven():
+        num=int(input("enter the number"))
+        if((num%2)==1):
+            print("odd number")
+            message="odd number"
+        else:
+            print("even number")
+            message="even number"
+            return message 
+
+    def BMI():
+        BMI=int(input("enter the BMI index:"))
+        if(BMI<18.5):
+            print("underweight")
+            message="underweight"
+        elif(BMI<24.9):
+            print("normal")
+            message="normal"
+        elif(BMI<29.9):
+            print("overweight")
+            message="overweight"
+        else:
+            print("very overweight")
+            message="very overweight"
+            return messages
+
+
+
+
+
+
